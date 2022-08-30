7fbec095a81e7bcc2b55c1f7740437b039e72b52
Shekhar Dahiya <dahiya.shekhar5@gmail.com>
2022-08-30 10:27:37 +0530

    changed server js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const connectDB = require("./config/db");
-const group= require('./routes/groups')
-const transaction= require('./routes/transaction')
-const user= require('./routes/user')
+const group = require('./routes/groups')
+const transaction = require('./routes/transaction')
+const user = require('./routes/user')
 connectDB();
 const cors = require('cors');
 
@@ -12,19 +12,21 @@ app.use(express.json());
 const PORT = process.env.PORT || 5000;
 
 
-app.use("/api/v1/bills",group)
-app.use("/api/v1/bills",transaction)
-app.use("/api/v1/bills",user)
+app.use("/api/v1/bills", group)
+app.use("/api/v1/bills", transaction)
+app.use("/api/v1/bills", user)
 
+app.get("/", (req, res) => {
+  res.send("Hello world")
+})
 
 const server = app.listen(
-    PORT,
-    console.log(`Server running on port
+  PORT,
+  console.log(`Server running on port
   ${PORT}`)
-  );
-  
-  process.on("unhandledRejection", (err, promise) => {
-    console.log(`Error:${err.message}`);
-    server.close(() => process.exit(1));
-  });
-  
+);
+
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error:${err.message}`);
+  server.close(() => process.exit(1));
+});
